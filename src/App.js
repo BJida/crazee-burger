a890ebdeaa185bdbb0fed26238809f3232186e80
Bassem JIDA <bassemjida@hotmail.com>
2025-12-21 21:29:22 +0100

    ✨ F02 : App > Install routing system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./Components/pages/login/LoginPage";
+import OrderPage from "./Components/pages/order/OrderPage";
+import ErrorPage from "./Components/pages/error/ErrorPage";
 
 
 function App() {
   return (
-    <div>
-      <LoginPage/>
-    </div>
+    
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/order" element={<OrderPage/>}/>
+        <Route path="*" element={< ErrorPage/>}/>
+      </Routes>
+ 
   );
 }
 
