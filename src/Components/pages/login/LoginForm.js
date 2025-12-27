e26ec14d7dfea30c647344a936d7d91295746bb3
Bassem JIDA <bassemjida@hotmail.com>
2025-12-27 23:57:34 +0100

    💄F03-live_AMA : 4 méthodes pour ajouter des styles

--- src/Components/pages/login/LoginForm.js
+++ src/Components/pages/login/LoginForm.js
@@ -1,6 +1,7 @@
 
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import "./LoginForm.css";
 export default function LoginForm() {
   // State
   const [inputValue, setInputValue] = useState("");
@@ -17,12 +18,13 @@ const handleSubmit  = (event) => {
  const handleChange = (event) => { 
     setInputValue(event.target.value);
   }
+  const titreH2Style={backgroundColor:"blue", color:"white", fontSize:"15px"};
   // Affichage (render)
     return (
     <form action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
+      <h1 style={{color:"red"}}>Bienvenue chez nous !</h1>
       <br />
-      <h2>Connectez-vous</h2>
+      <h2  className='bonbon'>Connectez-vous</h2>
         <input value={inputValue} onChange={handleChange} type="text" placeholder='Entrez votre prénom...' required />
         <button>Accédez à votre espace</button>
         {/* <Link to='/order'>vers OrderPage</Link> */}
@@ -31,3 +33,14 @@ const handleSubmit  = (event) => {
     </form>
   )
 }
+
+/** 4 méthodes pour ajouter du style à un composant 
+ * 1. inline style
+ * 2. object style
+ * 3. modules CSS (className)
+ * 4. global style (index.css)
+*/
+/**Styled-Components
+ * éviter le style collision (CSS, Sass)
+ * conserver le nested styling
+ */
