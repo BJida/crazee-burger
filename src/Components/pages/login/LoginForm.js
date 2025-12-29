09fce4b016217bf966fa8c6da322e88be6450e94
Bassem JIDA <bassemjida@hotmail.com>
2025-12-29 09:20:35 +0100

    🔥 F03 (live-AMA) :LoginForm >reset LoginForm before Styled Components

--- src/Components/pages/login/LoginForm.js
+++ src/Components/pages/login/LoginForm.js
@@ -1,7 +1,7 @@
 
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import "./LoginForm.css";
+
 export default function LoginForm() {
   // State
   const [inputValue, setInputValue] = useState("");
@@ -18,15 +18,24 @@ const handleSubmit  = (event) => {
  const handleChange = (event) => { 
     setInputValue(event.target.value);
   }
-  const titreH2Style={backgroundColor:"blue", color:"white", fontSize:"15px"};
+  //const titreH2Style={backgroundColor:"blue", color:"white", fontSize:"15px"};
   // Affichage (render)
     return (
     <form action="submit" onSubmit={handleSubmit}>
-      <h1 style={{color:"red"}}>Bienvenue chez nous !</h1>
-      <br />
-      <h2  className='bonbon'>Connectez-vous</h2>
-        <input value={inputValue} onChange={handleChange} type="text" placeholder='Entrez votre prénom...' required />
-        <button>Accédez à votre espace</button>
+      
+        <div >
+          <h1>Bienvenue chez nous !</h1>
+          <br />
+          <h2>Connectez-vous</h2>
+        </div>
+      
+        <div>          
+          <input value={inputValue} 
+          onChange={handleChange} 
+          type="text" 
+          placeholder='Entrez votre prénom' required />
+          <button>Accéder à mon espace</button>
+        </div>
         {/* <Link to='/order'>vers OrderPage</Link> */}
 
       
@@ -34,11 +43,11 @@ const handleSubmit  = (event) => {
   )
 }
 
+
 /** 4 méthodes pour ajouter du style à un composant 
  * 1. inline style
  * 2. object style
- * 3. modules CSS (className)
- * 4. global style (index.css)
+ * 3. modules CSS (classNam>.css)
 */
 /**Styled-Components
  * éviter le style collision (CSS, Sass)
