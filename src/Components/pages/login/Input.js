6e0bb8519d6c645b2eed8ab02ba800525c0a1fa1
Bassem JIDA <bassemjida@hotmail.com>
2025-12-30 17:23:28 +0100

    ♻️F03: LOGIN>LoginForm>Create reusable <Input/>

---
+++ src/Components/pages/login/Input.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export default function Input({value, onChange,Icon, ...extraProps}) {
+console.log("restProps: ", extraProps)
+  return (
+     <InputStyled>
+                {Icon && Icon}
+                <input 
+                value={value}
+                onChange={onChange}
+                type="text"
+                {...extraProps}
+                />
+              </InputStyled>
+  )
+}
+
+const InputStyled = styled.div`
+  /* border: 1px solid red; */
+  background-color: #fff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  padding: 18px 24px;
+  margin: 18px 20px;
+
+  .icon{
+    font-size:15px ;
+    margin-right: 8px;
+    color: #93a2b1;
+  }
+  input{
+    border: none;
+    font-size: 15px;
+    color: #17161a;
+    /* width: 100%;
+    display: flex; */
+  }
+  &::placeholder{
+    background: white;
+    color: lightgray;
+  }
+ 
+  
+`;
