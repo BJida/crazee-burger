086c0c808cdcd16e439daaf16d03f7948d9cc5cc
Bassem JIDA <bassemjida@hotmail.com>
2026-01-03 22:10:03 +0100

    🚧F05 : ORDER>Main>Add grid layout

--- src/Components/pages/order/Main.js
+++ src/Components/pages/order/Main.js
@@ -3,14 +3,29 @@ import { theme } from "../../../theme";
 
 export default function Main() {
   return (
-    <MainStyled className="main">Main</MainStyled>
+    <MainStyled className="main">
+      <div className="basket">basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
   )
 }
 const MainStyled = styled.div`
       background: ${theme.colors.background_white};
+      border: 5px solid green;
       flex: 1;/* or you can also use this :height: calc(95vh - 10vh);*/ 
+      display: flex;
 
       border-bottom-right-radius:${theme.borderRadius.extraRound};
       border-bottom-left-radius:${theme.borderRadius.extraRound}; 
       box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+
+      display: grid;
+      grid-template-columns: 25% 1fr;
+      .basket{
+        background: pink;
+      }
+
+      .menu{
+       background: purple;
+      }
 `;
