f70ca73163eaa0fdf58c7bbe6917f942cd9b3386
Bassem JIDA <bassemjida@hotmail.com>
2026-01-04 20:53:39 +0100

    💄F05 : ORDER>PRODUCT>Add image layout

--- src/Components/pages/order/Main/Menu.js
+++ src/Components/pages/order/Main/Menu.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { fakeMenu1 } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
 import Product from "./Product";
 
 
 export default function Menu() {
     // state
-    const [menu, setMenu] = useState(fakeMenu2);
+    const [menu, setMenu] = useState(fakeMenu1);
+
   return (
     <MenuStyled>
         {menu.map((product)=>{
